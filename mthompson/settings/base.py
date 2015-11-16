78367deb761904575543494afd7fbbcb88795a4d
Shane Taylor <shaneallantaylor@gmail.com>
2015-11-16 11:29:03 -0800

    added dj-database-url, dj-static, and gunicorn to settings and requirements - COULD BREAK

--- mthompson/settings/base.py
+++ mthompson/settings/base.py
@@ -117,6 +117,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mthompson',
+    'gunicorn',
     'photologue',
     'sortedm2m',
     'south',
--- mthompson/wsgi.py
+++ mthompson/wsgi.py
@@ -15,14 +15,16 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
     "mthompson.settings.dev")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
